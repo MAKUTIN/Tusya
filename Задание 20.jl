b60fad45ba677aba6c0b005e7548d2acf37677bc
MAKUTIN <144596670+MAKUTIN@users.noreply.github.com>
2023-12-14 23:35:10 +0300

    Create Задание 20.jl

---
+++ Задание 20.jl
@@ -0,0 +1,25 @@
+using HorizonSideRobots
+r = Robot("untitled.sit", animate = true)
+
+function recursion_along!(r::Robot, side::HorizonSide, steps::Int)
+    if !isborder(r, side)
+        move!(r, side)
+        steps += 1
+        steps += recursion_along!(r, side, steps)
+        return steps
+    else 
+        putmarker!(r)
+        num_move()
+        end
+        return 0
+    end
+end
+function num_move()
+  for i in 1:steps
+    move!(r, inverse(side))
+end
+function inverse(s)
+    return HorizonSide(mod(Int(s) + 2,4))
+end
+
+recursion_along!(r, Sud, 0)
