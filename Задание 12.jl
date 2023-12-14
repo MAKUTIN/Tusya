e0edeb4492e535e7e5643705d08421bdfd0dbdff
MAKUTIN <144596670+MAKUTIN@users.noreply.github.com>
2023-12-14 15:45:09 +0300

    Update Задание 12.jl

--- Задание 12.jl
+++ Задание 12.jl
@@ -1,78 +1,77 @@
 using HorizonSideRobots
-r = Robot("untitled.sit", animate=true) 
+robot = Robot(animate = true)
 
-function peregorodki!(rob)
-    g, v = first_step(rob, West), first_step(rob, Sud)
-    if ! isborder(rob, Nord)
-        move!(rob, Nord)
-    else
-        return 0
-    end
-    m = smake(rob)
-    first_step(rob, West)
-    first_step(rob, Sud)
-    num_move!(rob, Nord, v)
-    num_move!(rob, Ost, g)
-    return m
-end
-
-function stenki_d(rob, s)
-    x = 0
-    g = 0
-    f = 0
-    n = 0
-    while ! isborder(r, s)
-        n = g
-        g = f
-        if isborder(r, Sud)
-            f = 1
-        else
-            f = 0
+function num_borders!(robot::Robot)
+    side = Ost
+    N = 0
+    n_nord,n_ost = goangle_and_count!(robot,Sud,West)
+    while !isborder(robot,Nord) || !isborder(robot,Ost)
+        N += num_borders!(robot,side,Nord)
+        if !isborder(robot,Nord)
+            move!(robot,Nord)
         end
-        if  (f == 0) & (g == 0)
-            x += n  
+        side = inverse(side)
+    end
+    goangle_and_count!(robot,Sud,West)
+    goback!(robot,n_nord,n_ost)
+
+    return N-1  
+end
+
+function num_borders!(robot::Robot,move_side::HorizonSide,check_side::HorizonSide)
+    num_b = 0
+    state = 0 
+    while !isborder(robot,move_side)
+        if state == 0
+            if isborder(robot,check_side)
+                num_b += 1
+                state = 1
+            end
+        elseif state == 1
+            if isborder(robot,check_side)
+                state = 1
+            else
+                state = 2
+            end
+        elseif state == 2
+            if isborder(robot,check_side)
+                state = 1
+            else
+                state = 0
+            end
         end
-        move!(r, s)
+        move!(robot,move_side)
     end
-    if (f == 1) || (g == 1)
-        x += 1
-    end
-    println(x)
-    return x
+    return num_b
 end
 
-function num_move!(rob, side, n)
-    for i in 1:n
-        move!(rob, side)
+function move_ifnoborder!(robot::Robot, side::HorizonSide)
+    num_s  = 0
+    while !isborder(robot,side)
+        move!(robot,side)
+        num_s += 1
+    end
+    return num_s
+end
+
+function goangle_and_count!(robot::Robot,side1::HorizonSide,side2::HorizonSide)
+    num_s1 = num_s2 = 0
+    while !isborder(robot, side1) || !isborder(robot,side2)
+        num_s1 += move_ifnoborder!(robot,side1)
+        num_s2 += move_ifnoborder!(robot,side2)
+    end
+    return (num_s1, num_s2)
+end
+
+function goback!(robot::Robot, n_1::Int, n_2::Int)
+    for _ in range(0,n_1-1)
+        move!(robot, Nord)
+    end
+    for _ in range(0,n_2-1)
+        move!(robot,Ost)
     end
 end
 
-function smake(rob)
-    n = 0
-    s = Ost
-    while !isborder(rob,Nord)
-        x = stenki!(rob, s)
-        move!(rob, Nord)
-        s = inverse(s)
-        n += x
-    end
-    n += stenki!(rob, s)
-    return n
-end
+inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2,4))
 
-
-
-function inverse(s)
-    return HorizonSide(mod(Int(s) + 2,4))
-end
-
-function first_step(rob, side)
-    n = 0
-    while ! isborder(rob, side)
-        move!(rob, side)
-        n += 1
-    end
-    return n
-end
-
-print(peregorodki!(r))
+println(num_borders!(robot))
